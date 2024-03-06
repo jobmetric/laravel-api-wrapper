30872efef018974e4138282eff1ad9d6989e0f07
majid mohammadian <majeedmohammadian@gmail.com>
2024-03-07 02:34:01 +0330

    add service

---
+++ src/ApiWrapper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace src;
+
+use Illuminate\Contracts\Foundation\Application;
+
+class ApiWrapper
+{
+    /**
+     * The application instance.
+     *
+     * @var Application
+     */
+    protected Application $app;
+
+    /**
+     * Create a new Setting instance.
+     *
+     * @param Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+}
